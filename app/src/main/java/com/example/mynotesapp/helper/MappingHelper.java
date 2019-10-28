f5e60cd70a9a540690026ca0901720741d2f9b59
yadin.tiens@gmail.com <yadin.tiens@gmail.com>
2019-10-28 10:31:33 +0800

    First Commit

---
+++ app/src/main/java/com/example/mynotesapp/helper/MappingHelper.java
@@ -0,0 +1,27 @@
+package com.example.mynotesapp.helper;
+
+import android.database.Cursor;
+
+import com.example.mynotesapp.db.DatabaseContract;
+import com.example.mynotesapp.entity.Note;
+
+import java.util.ArrayList;
+import java.util.Currency;
+
+public class MappingHelper {
+
+    public static ArrayList<Note> mapCursorToArrayList(Cursor notesCursor) {
+        ArrayList<Note> notesList = new ArrayList<>();
+
+        while (notesCursor.moveToNext()) {
+            int id = notesCursor.getInt(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID));
+            String title = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE));
+            String description = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION));
+            String date = notesCursor.getString(notesCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE));
+            notesList.add(new Note(id, title, description, date));
+        }
+
+        return notesList;
+
+    }
+}
